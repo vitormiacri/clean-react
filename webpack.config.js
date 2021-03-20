684e1ddb87b4dbf63930d4eb92518022488fbf29
Vitor Morelli <vitao.miacri@gmail.com>
2021-03-20 11:46:07 -0300

    fix: adjust webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
-    fileName: 'bundle.js',
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'scss'],
@@ -49,7 +49,7 @@ module.exports = {
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDom',
+    'react-dom': 'ReactDOM',
   },
   plugins: [new CleanWebpackPlugin()],
 };
