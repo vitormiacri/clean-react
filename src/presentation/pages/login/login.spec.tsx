c1578619fbe6ac3ab3e5c3d3e9a76afe8adef890
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-12 14:15:11 -0300

    test: ensure Login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -23,7 +23,7 @@ type SutParams = {
   validationError: string;
 };
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({ initialEntries: ['/login'] });
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
@@ -184,6 +184,8 @@ describe('Login Component', () => {
       'accessToken',
       authenticationSpy.account.accessToken
     );
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe('/');
   });
 
   test('Should go to signup page', () => {
