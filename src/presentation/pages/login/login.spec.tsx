dd80881d0e0646e0bcacb870a5dffad51d304675
Vitor Morelli <vitao.miacri@gmail.com>
2021-04-12 09:59:11 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -134,4 +134,11 @@ describe('Login Component', () => {
       password,
     });
   });
+
+  test('Should call Ahthentication only once', () => {
+    const { sut, authenticationSpy } = makeSut();
+    simulateValidSubmit(sut);
+    simulateValidSubmit(sut);
+    expect(authenticationSpy.callsCount).toBe(1);
+  });
 });
