ec6d4c85e9b0752ac4f70f1966fb732d7f3009bf
Vitor Morelli <vitao.miacri@gmail.com>
2021-06-19 17:09:23 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { mockAxios } from '@/infra/test';
+import { mockAxios, mockHttpResponse } from '@/infra/test';
 import { mockedPostRequest } from '@/data/test';
 import { AxiosHttpClient } from './axios-http-client';
 
@@ -29,6 +29,15 @@ describe('AxiosHttpClient', () => {
 
   test('Should return the correct statusCode and body', () => {
     const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockedPostRequest());
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+  });
+
+  test('Should return the correct statusCode and body on failure', () => {
+    const { sut, mockedAxios } = makeSut();
+    mockedAxios.post.mockRejectedValueOnce({
+      response: mockHttpResponse(),
+    });
     const promise = sut.post(mockedPostRequest());
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
   });
--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,3 +1,3 @@
 export const makeApiUrl = (): string => {
-  return 'http://fordevs.herokuapp/api/login';
+  return 'http://fordevs.herokuapp.com/api/login';
 };
