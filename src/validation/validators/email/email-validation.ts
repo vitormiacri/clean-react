84d2642458ae82b7c14cca2bc76e120664c9c484
Vitor Morelli <vitao.miacri@gmail.com>
2021-06-21 14:27:01 -0300

    refactor: update eslint

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,7 +5,8 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error {
-    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return !value || emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
