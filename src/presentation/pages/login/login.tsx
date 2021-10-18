89901b57e77c4e8d59e0d77cdd7b6116e18d0414
Vitor Morelli <vitao.miacri@gmail.com>
2021-10-18 10:08:17 -0300

    refactor: change link name

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -84,7 +84,7 @@ const Login: React.FC<Props> = ({
           >
             Entrar
           </button>
-          <Link data-testid="signup" to="/signup" className={Styles.link}>
+          <Link data-testid="signup-link" to="/signup" className={Styles.link}>
             Criar conta
           </Link>
           <FormStatus />
