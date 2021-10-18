bb2efa50b9ec3d75135d4c10eed30a7ba0e2544f
Vitor Morelli <vitao.miacri@gmail.com>
2021-10-18 08:15:32 -0300

    feat: ensure SignUp change password status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -20,6 +20,7 @@ const SignUp: React.FC<Props> = ({ validation }) => {
     nameError: '',
     email: '',
     emailError: '',
+    password: '',
     passwordError: 'Campo Obrigatório',
     passwordConfirmationError: 'Campo Obrigatório',
     mainError: '',
@@ -29,8 +30,9 @@ const SignUp: React.FC<Props> = ({ validation }) => {
       ...state,
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password),
     });
-  }, [state.name, state.email]);
+  }, [state.name, state.email, state.password]);
 
   return (
     <div className={Styles.signup}>
