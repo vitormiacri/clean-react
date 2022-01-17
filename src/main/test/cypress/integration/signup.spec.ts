8d3dfab036f458e1563fbdc3bb621082f2d8a588
Vitor Morelli <vitao.miacri@gmail.com>
2022-01-17 10:11:37 -0300

    test: ensure SignUp present correct if email is in use

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,4 +1,17 @@
+import faker from 'faker';
 import * as FormHelper from '../support/form-helper';
+import * as Http from '../support/signup-mocks';
+
+const simulateValidSubmit = (): void => {
+  cy.getByTestId('name').focus().type(faker.name.findName());
+  cy.getByTestId('email').focus().type(faker.internet.email());
+  const password = faker.random.alphaNumeric(7);
+  cy.getByTestId('password').focus().type(password);
+  FormHelper.testInputStatus('password');
+  cy.getByTestId('passwordConfirmation').focus().type(password);
+  FormHelper.testInputStatus('passwordConfirmation');
+  cy.getByTestId('submit').click();
+};
 
 describe('SignUp', () => {
   beforeEach(() => {
@@ -16,5 +29,41 @@ describe('SignUp', () => {
     FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório');
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(2));
+    FormHelper.testInputStatus('name', 'Valor inválido');
+    cy.getByTestId('email').focus().type(faker.random.word());
+    FormHelper.testInputStatus('email', 'Valor inválido');
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3));
+    FormHelper.testInputStatus('password', 'Valor inválido');
+    cy.getByTestId('passwordConfirmation')
+      .focus()
+      .type(faker.random.alphaNumeric(4));
+    FormHelper.testInputStatus('passwordConfirmation', 'Valor inválido');
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('name').focus().type(faker.name.findName());
+    FormHelper.testInputStatus('name');
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    FormHelper.testInputStatus('email');
+    const password = faker.random.alphaNumeric(5);
+    cy.getByTestId('password').focus().type(password);
+    FormHelper.testInputStatus('password');
+    cy.getByTestId('passwordConfirmation').focus().type(password);
+    FormHelper.testInputStatus('passwordConfirmation');
+    cy.getByTestId('submit').should('not.have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present EmailInUseError on 403', () => {
+    Http.mockEmailInUseError();
+    simulateValidSubmit();
+    FormHelper.testMainError('Este e-mail já está em uso');
+    FormHelper.testUrl('/signup');
   });
 });
